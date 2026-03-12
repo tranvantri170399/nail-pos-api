bfb29402cd5c8d72dc3cf6d9072a13a51f50edbc
tranvantri170399 <tranvantri.170399@gmail.com>
2026-03-12 23:52:50 +0700

    allow user get api

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors({
+    origin: '*', // hoặc chỉ định cụ thể: 'http://localhost:64980'
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+  });
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
